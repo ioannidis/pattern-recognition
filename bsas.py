8027ef1e82decacfff4a11559f9bcd069c56ecf6
Panos Ioannidis <panos277@hotmail.com>
2019-01-31 01:48:34 +0200

    bsas update

--- bsas.py
+++ bsas.py
@@ -8,15 +8,11 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
-# pd.set_option('display.max_columns', 500)
+# PANDAS ========================================================================
+theta_list = np.arange(24, 72, 0.5)
 
-#  TESTING PANDAS ========================================================================
-data = [[2.5, 3.5], [2.5, 2.5], [-1, -1.5], [0.8, 0.8]]
-theta_list = np.arange(24, 72, 0.1)
-print(theta_list)
-
-u_cols = ["user id" , "movie id" , "rating", "timestamp"]
-ratings = pd.read_csv('./u.data', sep='\t', names=u_cols, encoding='latin-1')
+r_cols = ["user id" , "movie id" , "rating", "timestamp"]
+ratings = pd.read_csv('./u.data', sep='\t', names=r_cols, encoding='latin-1')
 
 ratings = ratings.sort_values("user id")
 ratings = ratings.loc[(ratings["rating"] >= 4)]
@@ -24,49 +20,30 @@ ratings = ratings.loc[(ratings["rating"] >= 4)]
 # print(len(ratings))
 
 # Extract data using pandas
-u_cols = ["movie id" , "movie title", "release date" , "video release date" ,
+m_cols = ["movie id" , "movie title", "release date" , "video release date" ,
               "IMDb URL" , "unknown" , "Action" , "Adventure" , "Animation" ,
               "Children's" , "Comedy" , "Crime" , "Documentary"
               , "Drama" , "Fantasy" ,
               "Film-Noir" , "Horror" , "Musical" , "Mystery" , "Romance" , "Sci-Fi" ,
               "Thriller" , "War" , "Western"]
 
-movies = pd.read_csv('./u.item', sep='|', names=u_cols, encoding='latin-1')
+movies = pd.read_csv('./u.item', sep='|', names=m_cols, encoding='latin-1')
 movies = movies.iloc[:, [0] + list(range(6, 24))]
 # print(movies)
 
 newArr = pd.merge(ratings,movies, on='movie id')
 newArr = newArr.sort_values("user id")
 newArrOnlyRatings = newArr.iloc[:, [0] + list(range(4, 22))]
-print(newArrOnlyRatings)
-
-# data = np.genfromtxt('./data1N.csv', delimiter=',')
-
-# Data  values
-# result = newArrOnlyRatings.iloc[:, list(range(1, 19))]
-# data = result.values.tolist()
-# print(data[0])
-# end Data  values
-
+# print(newArrOnlyRatings)
 
 # Data sum values
 result = newArrOnlyRatings.groupby(["user id"]).sum()
 result = result.iloc[:, list(range(0, 18))]
-print(result)
+# print(result)
 data = result.values.tolist()
-# print(data)
 # end Data sum values
 
-# data = newArrOnlyRatings.values.tolist()
-# print(data)
-
-#print(newArrOnlyRatings.values.tolist())
-
-
-
-# data = movies.iloc[:,5:24].values.tolist()
-
-# END TESTING PANDAS ========================================================================
+# END PANDAS ========================================================================
 
 # BSAS =========================================================================================
 
@@ -102,7 +79,7 @@ def bsas(data, theta):
         # print("=======================================================================================")
 
     # print("clusters: " , clusters)
-    print("cluster centroids: ", cluster_centroids)
+    # print("cluster centroids: ", cluster_centroids)
     return [clusters, cluster_centroids]
 
 def calculate_centroid(cluster_size, centroid, vector):
@@ -121,9 +98,6 @@ def clusters_number_estimation(theta_list, data):
         bsas_result = bsas(data, theta)
         cluster_estimations.append([theta, len(bsas_result[0])])
 
-
-    # print(cluster_estimations)
-
     cluster_number_for_each_theta = [number[1] for number in cluster_estimations]
     fig, ax = plt.subplots()
     ax.plot(theta_list, cluster_number_for_each_theta )
@@ -135,8 +109,3 @@ def clusters_number_estimation(theta_list, data):
 
 clusters_number_estimation(theta_list, data)
 
-# TODO: TESTING
-# bsas([[2.5, 3.5], [2.5, 2.5], [-1, -1.5], [0.8, 0.8]], 2)
-# clusters_number_estimation(theta_list, [[2.5, 3.5], [2.5, 2.5], [-1, -1.5], [0.8, 0.8]])
-# clusters_number_estimation(theta_list, [[0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0], [0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0], [0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]])
-# bsas([[-1, -1.3], [0.7, 0.8], [2, 3], [2.5, 2.5]], 0.6)
